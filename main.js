74a8fe20d50b5330d861f386e6b215f7759ab369
wills blake <richwblake@gmail.com>
2022-02-15 08:50:39 -0800

    Added new feature

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Loaded!");
+})
